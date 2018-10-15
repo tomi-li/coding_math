3fa1407250d1c621cfd1a1e0c55df2a9eaef8fea
拖米 <lishuo.lishuo@alibaba-inc.com>
2018-10-15 14:30:16 +0800

    feature: vector

--- packages/vectory/vector.js
+++ packages/vectory/vector.js
@@ -1,50 +1,47 @@
-module.exports = {
-  _x: 0,
-  _y: 1,
+module.exports = class Vector {
 
-  create(x, y) {
-    const obj = Object.create(this);
-    obj.setX(x);
-    obj.setY(y);
-    return obj;
-  },
+  constructor(x = 0, y = 1) {
+    this._x = x;
+    this._y = y;
+  }
+
+  static create(x, y) {
+    return new this(x, y);
+  }
 
   setX(x) {
     this._x = x;
-  },
+  }
 
   getX() {
     return this._x
-  },
+  }
 
   setY(y) {
     this._y = y;
-  },
+  }
 
   getY() {
     return this._y
-  },
+  }
 
   setLength(length) {
     const angle = this.getAngle();
     this._x = Math.cos(angle) * length;
     this._y = Math.sin(angle) * length;
-  },
+  }
 
   setAngle(angle) {
     const length = this.getLength();
     this._x = Math.cos(angle) * length;
     this._y = Math.sin(angle) * length;
-  },
+  }
 
   getLength() {
-    return Math.sqrt(this._x ** +this._y **);
-  },
+    return Math.sqrt(this._x ** 2 + this._y ** 2);
+  }
 
   getAngle() {
     return Math.atan2(this._y, this._x);
   }
-
 };
-
-
