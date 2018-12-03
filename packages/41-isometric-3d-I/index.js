bb496bc71c4ce6535509160683b76e3c9820fa1a
拖米 <lishuo.lishuo@alibaba-inc.com>
2018-12-03 11:14:28 +0800

    feature: add blocks

--- packages/41-isometric-3d-I/index.js
+++ packages/41-isometric-3d-I/index.js
@@ -20,11 +20,68 @@ window.onload = () => {
     ['blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue'],
   ];
 
-  tiles.forEach((row, rowIndex) => {
+  const blocks = [
+    ['1', '1', '1', '1', '1', '1', '1', '1'],
+    ['1', '1', '1', '1', '1', '1', '1', '1'],
+    ['1', '1', '1', '2', '2', '1', '1', '1'],
+    ['1', '1', '1', '2', '2', '1', '1', '1'],
+    ['1', '1', '1', '2', '2', '1', '1', '1'],
+    ['1', '1', '1', '1', '1', '1', '1', '1'],
+    ['1', '1', '1', '1', '1', '1', '1', '1'],
+  ];
+
+  blocks.forEach((row, rowIndex) => {
     row.forEach((color, columnIndex) => {
-      tile(rowIndex, columnIndex, color);
+      // tile(rowIndex, columnIndex, color);
+      drawBlock(rowIndex, columnIndex, Math.floor(Math.random() * 4))
     })
   });
+
+
+  function drawBlock(x, y, z) {
+    const
+      top = '#eeeeee',
+      left = '#cccccc',
+      right = '#999999';
+    const comparingPoint = z * tileHeight;
+
+    context.save();
+    context.translate(width / 2 + (x - y) * tileWidth / 2, 50 + (y + x) * tileHeight / 2 + 100);
+
+    // draw top
+    context.beginPath();
+    context.moveTo(0, -comparingPoint);
+    context.lineTo(tileWidth / 2, tileHeight / 2 - comparingPoint);
+    context.lineTo(0, tileHeight - comparingPoint);
+    context.lineTo(-tileWidth / 2, tileHeight / 2 - comparingPoint);
+    context.closePath();
+    context.fillStyle = top;
+    context.fill();
+
+    // draw left
+    context.beginPath();
+    context.moveTo(-tileWidth / 2, tileHeight / 2 - comparingPoint);
+    context.lineTo(-tileWidth / 2, tileHeight / 2);
+    context.lineTo(0, tileHeight);
+    context.lineTo(0, tileHeight - comparingPoint);
+    context.closePath();
+    context.fillStyle = left;
+    context.fill();
+
+    // draw left
+    context.beginPath();
+    context.moveTo(tileWidth / 2, tileHeight / 2 - comparingPoint);
+    context.lineTo(tileWidth / 2, tileHeight / 2);
+    context.lineTo(0, tileHeight);
+    context.lineTo(0, tileHeight - comparingPoint);
+    context.closePath();
+    context.fillStyle = right;
+    context.fill();
+
+
+    context.restore();
+  }
+
 
   function tile(x, y, color) {
     context.save();
